8ce44a9a26dfba948eefec3e0a1e5a7558327268
Marco Virgadamo <m.virgadamo@gmail.com>
2014-10-24 10:02:00 +0200

    pagine per test localizzazione

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
+
 app.get('/', function(req, res){
   res.sendFile('index.html', { root: __dirname });
 });
 
+app.get('/admin', function(req, res){
+  res.sendFile('admin.html', { root: __dirname });
+});
+
+app.get('/client', function(req, res){
+  res.sendFile('client.html', { root: __dirname });
+});
 
 
 http.listen(3000, function(){
