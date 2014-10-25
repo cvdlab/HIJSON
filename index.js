84e6a0c5f3d066847e752781ce9e491d4783267d
Marco Sportillo <marcosportillo@mbpms.lan>
2014-10-26 01:42:58 +0200

    posizioni e aggiornamento gitIgnore
    Aggiunto al gitignore il debug di npm;
    Posizioni versione raw funzionante

--- index.js
+++ index.js
@@ -2,59 +2,62 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var usersConnected = {};
 
 app.use(express.static(__dirname));
 
 app.get('/admins', function(req, res){
-  res.sendFile('admin.html', { root: __dirname });
+    res.sendFile('admin.html', { root: __dirname });
 });
 
 app.get('/users', function(req, res){
-  res.sendFile('user.html', { root: __dirname });
+    res.sendFile('user.html', { root: __dirname });
 });
 
 app.get('/', function(req, res){
-  res.sendFile('index.html', { root: __dirname });
+    res.sendFile('index.html', { root: __dirname });
 });
 
-
-
-// io.on('connection', function(socket){
-//   socket.on('position sent', function(coordinates){
-//     console.log('coordinates: ' + coordinates);
-//   });
-// });
 
 var admins = io.of('/admins');
-admins.on('connection', function(socket){
-  console.log('admin connected with id: ' + socket.id);
-   socket.on('disconnect', function() {
-  	console.log('admin disconnected with id: ' + socket.id)
-  });
-});
 
+admins.on('connection', function(socket){
+    console.log('admin connected with id: ' + socket.id);
+    admins.emit('updateUsers',usersConnected);
+  
+  socket.on('disconnect', function(){
+    console.log('admin disconnected with id: ' + socket.id);
+  });
+
+});
 
 var users = io.of('/users');
-users.on('connection', function(socket){
-  console.log('user connected with id: ' + socket.id);
-   socket.on('disconnect', function() {
-  	console.log('user disconnected with id: ' + socket.id)
-  });
-});
-
 
 users.on('connection', function(socket){
-  socket.on('sentCoordinates', function(latlng){
-    console.log('latlng: ' + latlng.lat + ';' + latlng.lng);
-    
-    var newPosition = {}
-    newPosition.id = socket.id;
-    newPosition.latlng = latlng;
-    users.emit('sentCoordinates', newPosition);
-    admins.emit('sentCoordinates', newPosition);
-  });
+    console.log('User connected with id: ' + socket.id);
+    var socketInfo = {
+        id: socket.id,
+        latlng: [0, 0]
+    };
+    usersConnected[socketInfo.id] = socketInfo;
+    socket.emit('initSocket', socketInfo);
+    admins.emit('updateUsers',usersConnected);
+
+    socket.on('disconnect', function(){
+        console.log('User disconnected with id: ' + socket.id);
+        delete usersConnected[socket.id];
+        admins.emit('updateUsers',usersConnected);
+    });
+
+    socket.on('sentCoordinates', function(latlng){
+        socketInfo.latlng = latlng;
+        console.log(socket.id +'[' + latlng.lat + ';' + latlng.lng + '].');
+        socket.emit('refreshCoordinates',socketInfo);
+        admins.emit('updateUsers',usersConnected);
+    });
 });
+
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
