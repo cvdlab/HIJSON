46f0c582abe71981c2b465dbd0b7df59261c4782
Marco Sportillo <marcosportillo@mbpms.local>
2015-01-23 18:03:41 +0100

    parsing completed. To fix camera3D

---
+++ public/libs/parser.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+
+function parseJSON(inputPaths) {
+	console.log('Starting C3D initialization...');
+    for (input_name in inputPaths)
+    {
+	    var parsedData = JSON.parse(fs.readFileSync(inputPaths[input_name], 'utf8'));
+	    
+	    process.stdout.write('Parsing: ' + input_name + '... ');
+	    
+	    if (input_name === 'config') {
+		    var config = parsedData;
+	    } else if(input_name === 'architecture') {
+	    	var architecture = parsedData;
+	    } else if(input_name === 'furniture') {
+	    	var furniture = parsedData;
+	    }
+		console.log('Done.');
+    }
+	var data = {
+		config: config,
+		input: {
+			architecture: architecture,
+			furniture: furniture
+		}
+	}
+	
+	return data;
+}
+
+module.exports = {
+	parse: parseJSON
+}
