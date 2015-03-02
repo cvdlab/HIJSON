07997a66c9c68bcb0318a8549dbd89b36ee6ddd2
Marco Virgadamo <m.virgadamo@gmail.com>
2015-03-02 20:17:06 +0100

    table as polygon

--- c3d/features/Table.js
+++ c3d/features/Table.js
@@ -7,48 +7,56 @@ function Table(feature) {
 }
 
 Table.prototype.style =	{
-							prefix: "fa",
-							icon: "square-o"
-    					};
+							"weight": 0,
+						    "fillColor": "#9b8c75",
+						    "fillOpacity": 1
+						};
 
 Table.prototype.get3DModel = function() {
 	var table = new THREE.Object3D();
+	
+	var tableX = this.geometry.coordinates[0][1][0];
+	var tableY = this.geometry.coordinates[0][2][1];
+	var tableZ = this.properties.height;
 
-	var geometry = new THREE.CylinderGeometry( 0.03, 0.03, 0.8, 32 );
+	var geometry = new THREE.CylinderGeometry( 0.03, 0.03, tableZ, 32 );
 	var material = new THREE.MeshLambertMaterial( {color: 0xd9d7d7} );
 
 	var p1 = new THREE.Mesh( geometry, material );
 	p1.name = "p1_" + this.id;
 	p1.rotation.x += Math.PI/2;
-	p1.position.z += 0.8/2;
+	p1.position.z += tableZ/2;
+	p1.position.x += 0.05;
+	p1.position.y += 0.05;
 
 	var p2 = new THREE.Mesh( geometry, material );
 	p2.name = "p2_" + this.id;
 	p2.rotation.x += Math.PI/2;
-	p2.position.z += 0.8/2;
-	p2.position.y += 1;
+	p2.position.z += tableZ/2;
+	p2.position.x += tableX - 0.05;
+	p2.position.y += 0.05;
 
 	var p3 = new THREE.Mesh( geometry, material );
 	p3.name = "p3_" + this.id;
 	p3.rotation.x += Math.PI/2;
-	p3.position.z += 0.8/2;
-	p3.position.x += 2;
+	p3.position.z += tableZ/2;
+	p3.position.x += tableX - 0.05;
+	p3.position.y += tableY - 0.05;
 
 	var p4 = new THREE.Mesh( geometry, material );
 	p4.name = "p4_" + this.id;
 	p4.rotation.x += Math.PI/2;
-	p4.position.z += 0.8/2;
-	p4.position.y += 1;
-	p4.position.x += 2;
+	p4.position.z += tableZ/2;
+	p4.position.x += 0.05;
+	p4.position.y += tabley - 0.05;
 
-
-	var geometry = new THREE.BoxGeometry( 2.1, 1.1, 0.04 );
+	var geometry = new THREE.BoxGeometry( tableX, tableY, 0.04 );
 	var material = new THREE.MeshLambertMaterial( {color: 0x9b8c75} );
 	var plane = new THREE.Mesh( geometry, material );
 	plane.name = "plane_" + this.id;
-	plane.position.x -= 0.05 - 2.1/2;
-	plane.position.y -= 0.05 - 1.1/2;
-	plane.position.z += 0.8;
+	plane.position.x += tableX/2;
+	plane.position.y += tableY/2;
+	plane.position.z += tableZ;
 
 	table.add(p1);
 	table.add(p2);
